74961ab9bb9398458bc4abe596a7f85ee5eb3fa2
lipanski <florinelul@gmail.com>
2015-03-05 02:33:57 +0100

    Added Configuration. Refactored

--- lib/slow_down.rb
+++ lib/slow_down.rb
@@ -1,9 +1,10 @@
-require "redis"
 require "slow_down/version"
 require "slow_down/configuration"
 
 module SlowDown
   module_function
+
+  Timeout = Class.new(StandardError)
 
   def config
     Configuration.instance.tap do |c|
@@ -11,49 +12,18 @@ module SlowDown
     end
   end
 
-  def configure(resource_count: 5, seconds: 20, timeout: 5, retries: 100, retry_strategy: nil, raise_on_timeout: false)
-    self.resource_count = resource_count
-    self.seconds = seconds
-    self.timeout = timeout
-    self.retries = retries
+  def locks
+    config.locks
   end
 
   def redis
-    @redis ||= Redis.new(url: "redis://localhost:6379/0")
-  end
-
-  def resource_count=(count)
-    @resource_count = count
-  end
-
-  def seconds=(seconds)
-    @seconds = seconds.to_f
-  end
-
-  def timeout=(timeout)
-    @timeout = timeout.to_f
-  end
-
-  def retries=(retries)
-    @retries = retries
-  end
-
-  def miliseconds_per_resource
-    @miliseconds_per_resource ||= (@seconds * 1000 / @resource_count).round
-  end
-
-  def seconds_per_retry
-    @timeout / @retries
-  end
-
-  def resources
-    @resources ||= @resource_count.times.map { |i| "resource_#{i}" }
+    config.redis
   end
 
   def free?
-    resources.each do |key|
-      if redis.set(key, 1, px: miliseconds_per_resource, nx: true)
-        puts "#{key} locked for #{miliseconds_per_resource}"
+    locks.each do |key|
+      if redis.set(key, 1, px: config.miliseconds_per_request_per_lock, nx: true)
+        puts "#{key} locked for #{config.miliseconds_per_request_per_lock}ms"
         return true
       end
     end
@@ -62,16 +32,26 @@ module SlowDown
   end
 
   def wait
-    puts "sleeping #{seconds_per_retry}"
-    sleep(seconds_per_retry)
+    puts "sleeping #{config.seconds_per_retry}"
+    sleep(config.seconds_per_retry)
   end
 
   def run
-    expires_at = Time.now + @timeout
+    expires_at = Time.now + config.timeout
+
     begin
       return yield if free?
       wait
     end until Time.now > expires_at
-    puts "timeouted..."
+
+    raise Timeout if config.raise_on_timeout
   end
+end
+
+SlowDown.config do |c|
+  c.requests_per_second = 2
+  c.retries = 50
+  c.timeout = 2
+  c.raise_on_timeout = false
+  c.redis_url = "redis://localhost:6379/0"
 end
--- lib/slow_down/configuration.rb
+++ lib/slow_down/configuration.rb
@@ -1,4 +1,5 @@
 require "singleton"
+require "redis"
 
 module SlowDown
   class Configuration
@@ -9,7 +10,11 @@ module SlowDown
       timeout: 5,
       retries: 100,
       retry_strategy: :liniar,
-      raise_on_timeout: false
+      raise_on_timeout: false,
+      redis: nil,
+      redis_url: nil,
+      redis_namespace: :slow_down,
+      locks_count: nil
     }
 
     DEFAULTS.each do |key, default_value|
@@ -20,17 +25,45 @@ module SlowDown
 
       # Setters
       define_method("#{key}=") do |value|
-        @invalidate = true
-        instance_variable_set("@#{key}", value || default_value)
+        invalidate
+        instance_variable_set("@#{key}", value.nil? ? default_value : value)
+      end
+    end
+
+    def redis
+      @redis ||= Redis.new(url: redis_url || ENV.fetch("REDIS_URL"))
+    end
+
+    def locks_count
+      @locks_count ||= [1, requests_per_second.ceil].max
+    end
+
+    def locks
+      @locks ||= locks_count.times.map do |i|
+        [redis_namespace, "lock_#{i}"].compact.join(":")
       end
     end
 
     def miliseconds_per_request
-      @miliseconds_per_request ||= (1000 / requests_per_second).round
+      @miliseconds_per_request ||= 1000.0 / requests_per_second
+    end
+
+    def miliseconds_per_request_per_lock
+      @miliseconds_per_request_per_lock ||= (miliseconds_per_request * locks_count).round
     end
 
     def seconds_per_retry
       @seconds_per_retry ||= timeout.to_f / retries
+    end
+
+    private
+
+    def invalidate
+      @locks_count = nil
+      @locks = nil
+      @miliseconds_per_request = nil
+      @miliseconds_per_request_per_lock = nil
+      @seconds_per_retry = nil
     end
   end
 end
