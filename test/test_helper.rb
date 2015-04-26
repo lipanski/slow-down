583efff680e3f5affbe0985e741de97fa676fd85
lipanski <florin.lipan@blacklane.com>
2015-04-26 12:22:06 +0200

    Explicitly require tempfile for ruby-2.0.0 (?)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require "minitest/reporters"
 require "minitest/mock"
 require "minitest/stub_const"
 require "benchmark"
+require "tempfile"
 require "slow_down"
 
 MiniTest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
